ab3e71073f76ab6b6186e2c9af2e22b2bd738ffe
Darek <hryciuk.d@gmail.com>
2021-08-17 17:53:46 +0200

    Update helloWorldExternal.groovy
    modify windows script

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,9 @@
 def call(Map config = [:]) {
-    loadLinuxScript(name: 'hello-world.sh')
-    sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+    //for linux
+   // loadLinuxScript(name: 'hello-world.sh')
+   // sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
+    
+    //for Windows OS
+      loadScript(name: 'hello-world.bat')
+      bat "./hello-world.bat ${config.name} ${config.dayOfWeek}"
 }
