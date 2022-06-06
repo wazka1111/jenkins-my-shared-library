639c939e311c22e9bf87b02527ca25abc1358eec
Darek <hryciuk.d@gmail.com>
2022-06-06 14:27:19 +0200

    Update runTest.groovy

--- vars/runTest.groovy
+++ vars/runTest.groovy
@@ -1,34 +1,6 @@
 def call() { 
- pipeline {
-    agent {
-        docker {
-            image 'maven:3.8.1-adoptopenjdk-11'
-            args '-v /root/.m2:/root/.m2'
-        }
-    }
-    stages {
-        stage('Maven version') {
-            steps {
-                sh 'mvn --version'
-            }
-        }
-        stage('Build') {
-            steps {
-                sh 'mvn -B -DskipTests clean package'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh 'mvn test'
-            }
-            post {
-                always {
-                    junit 'target/surefire-reports/*.xml'
-                }
-            }
-        }
-    }
+sh 'mvn test'     
 }
-} 
+ 
 
 
